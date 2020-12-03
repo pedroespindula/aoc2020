8087c99d45ab1a824dc422845f4b51a79a50dfc6
Pedro Espíndula <joaopedro.jpse@gmail.com>
2020-12-02 23:30:19 -0300

    Day 1 - Add separation between part 1 and part 2

--- src/aoc/2020/1-day.clj
+++ src/aoc/2020/1-day.clj
@@ -9,14 +9,21 @@
 (defn possibilities [elements report]
   (combinations report elements))
 
-(defn solve [report]
+(defn solve [report n]
   (->> report
-       (possibilities 3)
+       (possibilities n)
        (filter sums-into-2020)
        (flatten)
        (reduce *)))
 
+(defn solve-1 [report]
+  (solve report 2))
+
+(defn solve-2 [report]
+  (solve report 3))
+
 (defn main []
-  (println (solve (read-input-int 1))))
+  (println (format "Part 1: %d", (solve-1 (read-input-int 1))))
+  (println (format "Part 2: %d", (solve-2 (read-input-int 1)))))
 
 (main)
